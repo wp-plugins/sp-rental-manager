368d5bc447816a23dadaa38f48f866a2da2d88e2
smartypants <smartypants@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-25 19:35:29 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sp-rental-manager/tags/1.1.0@617498 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function() {
+
+	
+	
+	
+	
+	jQuery('.sp_rm_change_image').bind('click', function() { 		
+		
+		jQuery('.sp_main_image').attr('src', jQuery(this).attr('href'));
+		return false;
+	})
+
+
+
+
+
+
+
+;});
