37463aafab025c04e75539911c21c2e85c00a9f3
smartypants <smartypants@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-24 14:44:26 +0000

    = 1.1.5 =
    * Added the ability to unpublish a listing
    
    git-svn-id: https://plugins.svn.wordpress.org/sp-rental-manager/tags/1.1.5@844562 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/admin.js
@@ -0,0 +1,7 @@
+	
+	
+	function sp_rm_add_feature(){
+		
+		jQuery('#sp_rm_feature_end').before('<div id="sp_rm_feature_main" class="sp_rm_feature">Feature Name <input type="text" name="features[]" value=""> Feature Value <input type="text" name="features_value[]" value=""></div>');
+	}
+	
